8cc05a6bdc341e013d11f79efd604e369e5343fb
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2022-02-28 13:08:31 +0100

    Make Lua version more similar to JS version

--- multitone.lua
+++ multitone.lua
@@ -1,7 +1,5 @@
 --- Module to generate a duotone effect.
 -- It also supports gradients of more than two colors.
-local multitone = {}
-
 local base = [[
 <filter id='$id'>
 	<feColorMatrix type="matrix" values=".375 .5 .125 0 0 .375 .5 .125 0 0 .375 .5 .125 0 0 0 0 0 1 0" />
@@ -13,18 +11,22 @@ local base = [[
 </filter>
 ]]
 
-function multitone.filter(colors)
+local function normal(number)
+	return math.min(1, math.max(0, math.floor(number*1000)/1000))
+end
+
+local function multitone(id, ...)
 	local components = {{}, {}, {}}
-	for i, color in ipairs(colors) do
-		components[1][i] = string.format("%.4f", color[1])
-		components[2][i] = string.format("%.4f", color[2])
-		components[3][i] = string.format("%.4f", color[3])
+	for i, color in ipairs({...}) do
+		components[1][i] = tostring(normal(color[1]))
+		components[2][i] = tostring(normal(color[2]))
+		components[3][i] = tostring(normal(color[3]))
 	end
 	local options = {
 		red = table.concat(components[1], ' ');
 		green = table.concat(components[2], ' ');
 		blue = table.concat(components[3], ' ');
-		id = colors.id or "multitone"
+		id = id
 	}
 	return (base:gsub("$%w+", function(key)
 		return options[key:sub(2)] or error("Key "..key.." not found in options!")
